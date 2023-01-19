f0d7cd981eeab5071e261d9b5a272aa7b23b34dc
leticea <nascimento.leticeah@gmail.com>
2023-01-19 11:15:06 -0300

    criando pastas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import GlobalStyle from "./styles/global";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
+
+  )
 };
 
 export default App;
