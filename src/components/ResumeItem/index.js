92ec605621c4d30e17101041f22fd0916bf82751
leticea <nascimento.leticeah@gmail.com>
2023-01-19 11:41:32 -0300

    criando os cards

--- src/components/ResumeItem/index.js
+++ src/components/ResumeItem/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import * as C from "./styles";
 
-const ResumeItem = () => {
-  return <div>ResumeItem</div>
+const ResumeItem = ({ title, Icon }) => {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderTitle>{title}</C.HeaderTitle>
+        <Icon />
+      </C.Header>
+      <C.Total>1000</C.Total>
+    </C.Container>
+  )
 };
 
 export default ResumeItem;
