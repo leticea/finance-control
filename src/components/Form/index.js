db63ebd25cb3b78b96fdd0e95dca8cc9d96b92fe
leticea <nascimento.leticeah@gmail.com>
2023-01-19 12:16:26 -0300

    criando os inputs

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import * as C from "./styles";
 
 const Form = () => {
@@ -24,10 +24,21 @@ const Form = () => {
           <C.Label>Descrição</C.Label>
           <C.Input value={desc} onChange={(e) => setDesc(e.target.value)} />
         </C.InputContent>
+        <C.InputContent>
+          <C.Label>Valor</C.Label>
+          <C.Input value={amount} type="number" onChange={(e) => setAmount(e.target.value)} />
+        </C.InputContent>
+        <C.RadioGroup>
+          <C.Input type="radio" id="rIncome" defaultChecked name="group1" onChange={() => setExpense(!isExpense)} />
+          <C.Label htmlFor="rIncome">Entrada</C.Label>
+          <C.Input type="radio" id="rExpenses" name="group1" onChange={() => setExpense(!isExpense)} />
+          <C.Label htmlFor="rExpenses">Saída</C.Label>
+        </C.RadioGroup>
+        <C.Button onClick={handleSave}>ADICIONAR</C.Button>
       </C.Container>
     </>
-  )
-}
+  );
+};
 
 
 
