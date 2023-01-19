a569dfd7258ad132a272662440052e36ffcfb4fa
leticea <nascimento.leticeah@gmail.com>
2023-01-19 14:05:32 -0300

    criando o metodo para deletar

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -3,6 +3,12 @@ import GridItem from "../GridItem";
 import * as C from "./styles";
 
 const Grid = ({ itens, setItens }) => {
+  const onDelete = (ID) => {
+    const newArray = itens.filter((transaction) => transaction.id !== ID);
+    setItens(newArray);
+    localStorage.setItem("transactions", JSON.stringify(newArray));
+  };
+  
   return (
     <C.Table>
       <C.Thead>
